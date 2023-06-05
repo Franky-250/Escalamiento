43ee8f9449e14670ec9b02592856a7a6aeca572a
Franky-250 <117626845+Franky-250@users.noreply.github.com>
2023-06-04 21:45:30 -0500

    Add files via upload

---
+++ EJERCICIO2/BACKEND-PROYECTO/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+// middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cors({
+    origin: '*'
+}))
+
+const tipoProyecto = require('./routes/tipoProyecto')
+const cliente = require('./routes/cliente')
+const etapas = require('./routes/etapas')
+const universidad = require('./routes/universidad')
+
+
+// middlewares
+app.use('/api/tiposproyectos', tipoProyecto)
+app.use('/api/clientes', cliente)
+app.use('/api/etapa', etapas)
+app.use('/api/universidades', universidad)
+
+module.exports = app
